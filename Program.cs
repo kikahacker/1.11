01ba5dbf02f29492eee0d9524a01a7d291f7199b
your_username <your_email_address@example.com>
2024-10-16 15:12:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,188 @@
+﻿/*Console.WriteLine("Введите дату рождения в формате xx.xx.xxxx");
+string bith = Console.ReadLine();
+if (bith.Length != 10 || (bith[2] != '.' && bith[5] != '.') || bith[7] < 9)
+{
+    Console.WriteLine("Неверный формат");
+}
+else
+{
+    string[] bith2 = bith.Split('.', 3);
+    int day = Convert.ToInt32(bith2[0]);
+    int month = Convert.ToInt32(bith2[1]);
+    int year = Convert.ToInt32(bith2[2]);
+    string[] zodiac = ["Козерог", "Водолей", "Рыбы", "Овен", "Телец", "Близнецы", "Рак", "Лев", "Дева", "Весы", "Скорпион", "Стрелец"];
+    string[] animals = ["Крыса", "Бык", "Тигр", "Кролик", "Дракон", "Змея", "Лошадь", "Обезьяна", "Петух", "Собака", "Кабан"];
+    string[] color = ["Металл", "Вода", "Дерево", "Огонь", "Земля"];
+    int x = year - 1900;
+    while (x >= 12)
+    {
+        x = x - 12;
+    }
+    int y = year - 1900;
+    while (y >= 10)
+    {
+        y = y - 10;
+    }
+    Console.WriteLine("Ваш знак зодиака:");
+
+    switch (month)
+    {
+        case 01:
+            if (day <= 20)
+            {
+                Console.WriteLine(zodiac[0]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[1]);
+            }
+            break;
+
+        case 02:
+            if (day <= 18)
+            {
+                Console.WriteLine(zodiac[1]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[2]);
+            }
+            break;
+
+        case 03:
+            if (day <= 20)
+            {
+                Console.WriteLine(zodiac[2]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[3]);
+            }
+            break;
+
+        case 04:
+            if (day <= 20)
+            {
+                Console.WriteLine(zodiac[3]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[4]);
+            }
+            break;
+
+        case 05:
+            if (day <= 20)
+            {
+                Console.WriteLine(zodiac[4]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[5]);
+            }
+            break;
+
+        case 06:
+            if (day <= 21)
+            {
+                Console.WriteLine(zodiac[5]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[6]);
+            }
+            break;
+
+        case 07:
+            if (day <= 22)
+            {
+                Console.WriteLine(zodiac[6]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[7]);
+            }
+            break;
+
+        case 08:
+            if (day <= 22)
+            {
+                Console.WriteLine(zodiac[7]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[8]);
+            }
+            break;
+
+        case 09:
+            if (day <= 23)
+            {
+                Console.WriteLine(zodiac[8]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[9]);
+            }
+            break;
+
+        case 10:
+            if (day <= 23)
+            {
+                Console.WriteLine(zodiac[9]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[10]);
+            }
+            break;
+
+        case 11:
+            if (day <= 22)
+            {
+                Console.WriteLine(zodiac[10]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[11]);
+            }
+            break;
+
+        case 12:
+            if (day <= 21)
+            {
+                Console.WriteLine(zodiac[11]);
+            }
+            else
+            {
+                Console.WriteLine(zodiac[0]);
+            }
+            break;
+
+
+    }
+    Console.WriteLine("Ваше животное по китайскому каллендарю:");
+    if (x == 0)
+    {
+        string anim = animals[x];
+        Console.WriteLine(anim);
+    }
+    else
+    {
+        string anim = animals[x - 1];
+        Console.WriteLine(anim);
+    }
+    Console.WriteLine("Ваша стихия");
+    if (y > 4)
+    {
+        y = y / 2;
+        string col = color[y];
+        Console.WriteLine(col);
+    }
+    else 
+    {
+        string col = color[y];
+        Console.WriteLine(col);
+    }
+}
+*/
---
+++ task.cs
@@ -0,0 +1,171 @@
+﻿Console.WriteLine("Введите количество сколько прошло месяцев");
+string fa = Console.ReadLine();
+try 
+{ 
+    int n = Convert.ToInt32(fa);
+    if (n > 2500)
+    {
+        Console.WriteLine("ERROR");
+        
+    }
+    else
+    {
+        string start = "01.01.1990";
+        string[] start1 = start.Split('.', 3);
+        int day = Convert.ToInt32(start1[0]);
+        int month = Convert.ToInt32(start1[1]);
+        int kodye = 0;
+        int yearint = Convert.ToInt32(start1[2]);
+        while (n >= 12)
+        {
+            n = n - 12;
+            yearint = yearint + 1;
+        }
+        month = month + n;
+        int monthkod = 0;
+        string yearstr = Convert.ToString(yearint);
+        string k = Convert.ToString(yearstr[1]);
+        string[] ye = yearstr.Split(k, 2);
+        if (Convert.ToInt32(k) == 9)
+        {
+            kodye = 0;
+        }
+        if (Convert.ToInt32(k) == 0)
+        {
+            kodye = 6;
+        }
+        if (Convert.ToInt32(k) == 1)
+        {
+            kodye = 4;
+        }
+        int yearkod = (kodye + Convert.ToInt32(ye[1]) + Convert.ToInt32(ye[1]) / 4) % 7;
+        switch (month)
+        {
+            case 1:
+                {
+                    Console.WriteLine("Январь");
+                    if (DateTime.IsLeapYear(yearint) == true)
+                    {
+                        monthkod = 0;
+                    }
+                    else
+                    {
+                        monthkod = 1;
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    Console.WriteLine("Февраль");
+                    if (DateTime.IsLeapYear(yearint) == true)
+                    {
+                        monthkod = 3;
+                    }
+                    else
+                    {
+                        monthkod = 4;
+                    }
+                    break;
+                }
+            case 3:
+                {
+                    Console.WriteLine("Март");
+                    monthkod = 4;
+                    break;
+                }
+            case 4:
+                {
+                    Console.WriteLine("Апрель");
+                    monthkod = 0;
+                    break;
+                }
+            case 5:
+                {
+                    Console.WriteLine("Май");
+                    monthkod = 2;
+                    break;
+                }
+            case 6:
+                {
+                    Console.WriteLine("Июнь");
+                    monthkod = 5;
+                    break;
+                }
+            case 7:
+                {
+                    Console.WriteLine("Июль");
+                    monthkod = 0;
+                    break;
+                }
+            case 8:
+                {
+                    Console.WriteLine("Август");
+                    monthkod = 3;
+                    break;
+                }
+            case 9:
+                {
+                    Console.WriteLine("Сентябрь");
+                    monthkod = 6;
+                    break;
+                }
+            case 10:
+                {
+                    Console.WriteLine("Октябрь");
+                    monthkod = 1;
+                    break;
+                }
+            case 11:
+                {
+                    Console.WriteLine("Ноябрь");
+                    monthkod = 4;
+                    break;
+                }
+            case 12:
+                {
+                    Console.WriteLine("Декабрь");
+                    monthkod = 6;
+                    break;
+                }
+        }
+        if (day < 10)
+        {
+            Console.Write("0");
+        }
+        Console.Write(day);
+        Console.Write(".");
+        if (month < 10)
+        {
+            Console.Write("0");
+        }
+        Console.Write(month);
+        Console.Write(".");
+        Console.Write(yearint);
+        Console.WriteLine("");
+        int dayned = (day + monthkod + yearkod) % 7;
+        switch (dayned)
+        {
+            case 0: Console.WriteLine("Суббота"); break;
+            case 1: Console.WriteLine("Воскресенье"); break;
+            case 2: Console.WriteLine("Понедельник"); break;
+            case 3: Console.WriteLine("Вторник"); break;
+            case 4: Console.WriteLine("Среда"); break;
+            case 5: Console.WriteLine("Четверг"); break;
+            case 6: Console.WriteLine("Пятница"); break;
+        }
+        if (DateTime.IsLeapYear(yearint) == true)
+        {
+            Console.WriteLine("Год високосный");
+
+        }
+        else
+        {
+            Console.WriteLine("Год не високосный");
+        }
+    }
+} 
+catch (Exception e)
+{
+    Console.WriteLine("Ошибка");
+
+}
